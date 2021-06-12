shakahl/peer-calls

commit b35fdb6b1e43fa948cea58227032593e015b12b4

Add video quality option
Users can pick between Lo, Md, Sd and Hd.

Also fix a bug where remote stream was not being removed in mesh mode.

src/client/components/Media.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 import { MdError } from 'react-icons/md'
 import { connect } from 'react-redux'
 import { dial } from '../actions/CallActions'
-import { AudioConstraint, enumerateDevices, getMediaStream, MediaDevice, play, setAudioConstraint, setVideoConstraint, VideoConstraint } from '../actions/MediaActions'
+import { enumerateDevices, getDeviceId, getMediaStream, MediaDevice, MediaKind, play, setDeviceIdOrDisable, toggleDevice } from '../actions/MediaActions'
 import { error, info, warning } from '../actions/NotifyActions'
-import { DialState, DIAL_STATE_HUNG_UP, ME } from '../constants'
+import { DEVICE_DEFAULT_ID, DEVICE_DISABLED_ID, DialState, DIAL_STATE_HUNG_UP, ME } from '../constants'
 import { MediaState } from '../reducers/media'
 import { State } from '../store'
 import { config } from '../window'
@@ -21,8 +21,7 @@ export type MediaProps = MediaState & {
   dialState: DialState
   visible: boolean
   enumerateDevices: typeof enumerateDevices
-  onSetVideoConstraint: typeof setVideoConstraint
-  onSetAudioConstraint: typeof setAudioConstraint
+  setDeviceId: typeof setDeviceIdOrDisable
   getMediaStream: typeof getMediaStream
   play: typeof play
   logInfo: typeof info
@@ -51,8 +50,8 @@ function mapStateToProps(state: State) {
 const mapDispatchToProps = {
   enumerateDevices,
   dial,
-  onSetVideoConstraint: setVideoConstraint,
-  onSetAudioConstraint: setAudioConstraint,
+  toggleDevice,
+  setDeviceId: setDeviceIdOrDisable,
   getMediaStream,
   play,
   logInfo: info,
@@ -80,8 +79,22 @@ extends React.PureComponent<MediaProps, MediaComponentState> {
     event.preventDefault()
     const { props } = this
     const { audio, video } = props
+
+    const constraints: MediaStreamConstraints = {
+      audio: false,
+      video: false,
+    }
+
+    if (audio.enabled) {
+      constraints.audio = audio.constraints
+    }
+
+    if (video.enabled) {
+      constraints.video = video.constraints
+    }
+
     try {
-      await props.getMediaStream({ audio, video })
+      await props.getMediaStream(constraints)
     } catch (err) {
       this.setState({ error: true })
       return
@@ -98,26 +111,31 @@ extends React.PureComponent<MediaProps, MediaComponentState> {
     }
   }
   handleVideoChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const constraint: VideoConstraint = JSON.parse(event.target.value)
-    this.props.onSetVideoConstraint(constraint)
+    this.handleChange('video', event.target.value)
   }
 
   handleAudioChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const constraint: AudioConstraint = JSON.parse(event.target.value)
-    this.props.onSetAudioConstraint(constraint)
+    this.handleChange('audio', event.target.value)
   }
   handleNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({ nickname: event.target.value })
   }
+  handleChange = (kind: MediaKind, deviceId: string) => {
+    this.props.setDeviceId({
+      kind,
+      deviceId,
+    })
+  }
   render() {
     const { props } = this
+    const { audio, video } = props
     const { nickname } = this.state
     if (!props.visible) {
       return null
     }
 
-    const videoId = JSON.stringify(props.video)
-    const audioId = JSON.stringify(props.audio)
+    const videoId = getDeviceId(video.enabled, video.constraints)
+    const audioId = getDeviceId(audio.enabled, audio.constraints)
 
     return (
       <form className='media' onSubmit={this.handleSubmit}>
@@ -146,7 +164,7 @@ extends React.PureComponent<MediaProps, MediaComponentState> {
           >
             <Options
               devices={props.devices}
-              default='{"facingMode":"user"}'
+              default={DEVICE_DEFAULT_ID}
               type='videoinput'
             />
           </select>
@@ -161,7 +179,7 @@ extends React.PureComponent<MediaProps, MediaComponentState> {
           >
             <Options
               devices={props.devices}
-              default='true'
+              default={DEVICE_DEFAULT_ID}
               type='audioinput'
             />
           </select>
@@ -241,8 +259,8 @@ function Options(props: OptionsProps) {
   const label = labels[props.type]
   return (
     <React.Fragment>
-      <option value='false'>No {label}</option>
-      <option value={props.default}>Default {label}</option>
+      <option value={DEVICE_DISABLED_ID}>No {label}</option>
+      <option value={DEVICE_DEFAULT_ID}>Default {label}</option>
       {
         props.devices
         .filter(device => device.type === props.type)

src/client/reducers/media.ts
@@ -1,23 +1,42 @@
+import omit from 'lodash/omit'
 import { ConnectedAction, DialAction, DisconnectedAction, HangUpAction } from '../actions/CallActions'
-import { AudioConstraint, MediaAction, MediaDevice, MediaEnumerateAction, MediaPlayAction, MediaStreamAction, VideoConstraint } from '../actions/MediaActions'
-import { DIAL, DialState, DIAL_STATE_DIALLING, DIAL_STATE_HUNG_UP, DIAL_STATE_IN_CALL, HANG_UP, MEDIA_AUDIO_CONSTRAINT_SET, MEDIA_ENUMERATE, MEDIA_PLAY, MEDIA_STREAM, MEDIA_VIDEO_CONSTRAINT_SET, SOCKET_CONNECTED, SOCKET_DISCONNECTED } from '../constants'
+import { DeviceId, MediaAction, MediaDevice, MediaDeviceToggle, MediaEnumerateAction, MediaPlayAction, MediaStreamAction, SizeConstraint } from '../actions/MediaActions'
+import { DIAL, DialState, DIAL_STATE_DIALLING, DIAL_STATE_HUNG_UP, DIAL_STATE_IN_CALL, HANG_UP, MEDIA_DEVICE_ID, MEDIA_DEVICE_TOGGLE, MEDIA_ENUMERATE, MEDIA_PLAY, MEDIA_SIZE_CONSTRAINT, MEDIA_STREAM, SOCKET_CONNECTED, SOCKET_DISCONNECTED } from '../constants'
+
+
+
+export interface MediaConstraint {
+  constraints: MediaTrackConstraints
+  enabled: boolean
+}
 
 export interface MediaState {
   socketConnected: boolean
   devices: MediaDevice[]
-  video: VideoConstraint
-  audio: AudioConstraint
+  video: MediaConstraint
+  audio: MediaConstraint
   dialState: DialState
   loading: boolean
   error: string
   autoplayError: boolean
 }
 
+const defaultConstraints = {
+  video: { facingMode: 'user' },
+  audio: {},
+}
+
 const defaultState: MediaState = {
   socketConnected: false,
   devices: [],
-  video: { facingMode: 'user'},
-  audio: true,
+  video: {
+    enabled: true,
+    constraints: defaultConstraints.video,
+  },
+  audio: {
+    enabled: true,
+    constraints: defaultConstraints.audio,
+  },
   dialState: DIAL_STATE_HUNG_UP,
   loading: false,
   error: '',
@@ -115,6 +134,71 @@ export function handleDial(state: MediaState, action: DialAction): MediaState {
   }
 }
 
+export function handleDeviceToggle(
+  state: MediaState,
+  payload: MediaDeviceToggle,
+): MediaState {
+  const deviceState = {
+    ...state[payload.kind],
+    enabled: payload.enabled,
+  }
+
+  return {
+    ...state,
+    [payload.kind]: deviceState,
+  }
+}
+
+
+export function handleDeviceId(
+  state: MediaState,
+  payload: DeviceId,
+): MediaState {
+  let { constraints } = state[payload.kind]
+
+  if (payload.deviceId !== '') {
+    const defaultKeys = Object.keys(defaultConstraints[payload.kind])
+    constraints = omit(constraints, defaultKeys)
+    constraints.deviceId = payload.deviceId
+  } else {
+    constraints = omit(constraints, 'deviceId')
+    constraints = {...constraints, ...defaultConstraints[payload.kind]}
+  }
+
+  return {
+    ...state,
+    [payload.kind]: {
+      constraints,
+      enabled: true,
+    },
+  }
+}
+
+export function handleSizeConstraint(
+  state: MediaState,
+  payload: SizeConstraint | null,
+): MediaState {
+  let { constraints } = state.video
+
+  if (!payload) {
+    constraints = omit(constraints, 'width', 'height')
+  } else {
+    constraints = {
+      ...constraints,
+      width: payload.width,
+      height: payload.height,
+    }
+  }
+
+  return {
+    ...state,
+    video: {
+      ...state.video,
+      constraints,
+    },
+  }
+}
+
 export default function media(
   state = defaultState,
   action:
@@ -127,16 +211,12 @@ export default function media(
   switch (action.type) {
     case MEDIA_ENUMERATE:
       return handleEnumerate(state, action)
-    case MEDIA_AUDIO_CONSTRAINT_SET:
-      return {
-        ...state,
-        audio: action.payload,
-      }
-    case MEDIA_VIDEO_CONSTRAINT_SET:
-      return {
-        ...state,
-        video: action.payload,
-      }
+    case MEDIA_DEVICE_ID:
+      return handleDeviceId(state, action.payload)
+    case MEDIA_DEVICE_TOGGLE:
+      return handleDeviceToggle(state, action.payload)
+    case MEDIA_SIZE_CONSTRAINT:
+      return handleSizeConstraint(state, action.payload)
     case MEDIA_STREAM:
       return handleMediaStream(state, action)
     case MEDIA_PLAY:

src/client/components/Media.test.tsx
@@ -11,6 +11,8 @@ import { MEDIA_ENUMERATE, ME, DIAL, DIAL_STATE_IN_CALL, DIAL_STATE_HUNG_UP } fro
 import { dial } from '../actions/CallActions'
 import { MediaStream } from '../window'
 
+import { MediaConstraint } from '../reducers/media'
+
 describe('Media', () => {
 
   beforeEach(() => {
@@ -130,28 +132,56 @@ describe('Media', () => {
   })
 
   describe('onVideoChange', () => {
-    it('calls onSetVideoConstraint', async () => {
+    it('calls setVideoConstraint', async () => {
       const node = await render()
       const select = node.querySelector('select[name=video-input]')!
-      TestUtils.Simulate.change(select, {
-        target: {
-          value: '{"deviceId":123}',
-        } as any,
+
+      const tests: {value: string, expected: MediaConstraint}[] = [{
+        value: 'disabled',
+        expected: {enabled: false, constraints: {facingMode: 'user'}},
+      }, {
+        value: '',
+        expected: {enabled: true, constraints: {facingMode: 'user'}},
+      }, {
+        value: 'abcd',
+        expected: {enabled: true, constraints: {deviceId: 'abcd'}},
+      }]
+
+      tests.forEach(test => {
+        TestUtils.Simulate.change(select, {
+          target: {
+            value: test.value,
+          } as any,
+        })
+        expect(store.getState().media.video).toEqual(test.expected)
       })
-      expect(store.getState().media.video).toEqual({ deviceId: 123 })
     })
   })
 
   describe('onAudioChange', () => {
-    it('calls onSetAudioConstraint', async () => {
+    it('calls setAudioConstraint', async () => {
       const node = await render()
-      const select = node.querySelector('select[name="audio-input"]')!
-      TestUtils.Simulate.change(select, {
-        target: {
-          value: '{"deviceId":456}',
-        } as any,
+      const select = node.querySelector('select[name=audio-input]')!
+
+      const tests: {value: string, expected: MediaConstraint}[] = [{
+        value: 'disabled',
+        expected: {enabled: false, constraints: {}},
+      }, {
+        value: '',
+        expected: {enabled: true, constraints: {}},
+      }, {
+        value: 'abcd',
+        expected: {enabled: true, constraints: {deviceId: 'abcd'}},
+      }]
+
+      tests.forEach(test => {
+        TestUtils.Simulate.change(select, {
+          target: {
+            value: test.value,
+          } as any,
+        })
+        expect(store.getState().media.audio).toEqual(test.expected)
       })
-      expect(store.getState().media.audio).toEqual({ deviceId: 456 })
     })
   })
 

src/client/actions/MediaActions.ts
@@ -1,8 +1,9 @@
 import _debug from 'debug'
 import { AsyncAction, makeAction } from '../async'
-import { MEDIA_AUDIO_CONSTRAINT_SET, MEDIA_ENUMERATE, MEDIA_STREAM, MEDIA_VIDEO_CONSTRAINT_SET, MEDIA_TRACK, MEDIA_TRACK_ENABLE } from '../constants'
+import { DEVICE_DISABLED_ID, MEDIA_ENUMERATE, MEDIA_STREAM, MEDIA_TRACK, MEDIA_TRACK_ENABLE, MEDIA_SIZE_CONSTRAINT, MEDIA_DEVICE_ID, MEDIA_DEVICE_TOGGLE, DEVICE_DEFAULT_ID } from '../constants'
 import { AddLocalStreamPayload, StreamTypeCamera, StreamTypeDesktop, StreamType } from './StreamActions'
 import { MediaStream } from '../window'
+import uniqBy from 'lodash/uniqBy'
 
 const debug = _debug('peercalls')
 
@@ -39,7 +40,7 @@ export const enumerateDevices = makeAction(MEDIA_ENUMERATE, async () => {
     stream.getTracks().forEach(track => track.stop())
   }
 
-  return devices
+  const mappedDevices = devices
   .filter(
     device => device.kind === 'audioinput' || device.kind === 'videoinput')
   .map(device => ({
@@ -48,25 +49,8 @@ export const enumerateDevices = makeAction(MEDIA_ENUMERATE, async () => {
     name: device.label,
   }) as MediaDevice)
 
+  return uniqBy(mappedDevices, item => item.id)
 })
-
-export type FacingMode = 'user' | 'environment'
-
-export interface DeviceConstraint {
-  deviceId: string
-}
-
-export interface FacingConstraint {
-  facingMode: FacingMode | { exact: FacingMode }
-}
-
-export type VideoConstraint = DeviceConstraint | boolean | FacingConstraint
-export type AudioConstraint = DeviceConstraint | boolean
-
-export interface GetMediaConstraints {
-  video: VideoConstraint
-  audio: AudioConstraint
-}
 
 declare global {
   interface Navigator {
@@ -110,32 +94,74 @@ async function getDisplayMedia(
   return mediaDevices.getDisplayMedia(constraints)
 }
 
-export interface MediaVideoConstraintAction {
-  type: 'MEDIA_VIDEO_CONSTRAINT_SET'
-  payload: VideoConstraint
+export interface SizeConstraint {
+  width: number
+  height: number
 }
 
-export interface MediaAudioConstraintAction {
-  type: 'MEDIA_AUDIO_CONSTRAINT_SET'
-  payload: AudioConstraint
+export interface MediaSizeConstraintAction {
+  type: 'MEDIA_SIZE_CONSTRAINT'
+  payload: SizeConstraint | null
 }
 
-export function setVideoConstraint(
-  payload: VideoConstraint,
-): MediaVideoConstraintAction {
+export interface DeviceId {
+  kind: MediaKind
+  deviceId: string
+}
+
+export interface MediaDeviceIdAction {
+  type: 'MEDIA_DEVICE_ID'
+  payload: DeviceId
+}
+
+export interface MediaDeviceToggle {
+  kind: MediaKind
+  enabled: boolean
+}
+
+export interface MediaDeviceToggleAction {
+  type: 'MEDIA_DEVICE_TOGGLE'
+  payload: MediaDeviceToggle
+}
+
+export function toggleDevice(
+  payload: MediaDeviceToggle,
+): MediaDeviceToggleAction {
   return {
-    type: MEDIA_VIDEO_CONSTRAINT_SET,
+    type: MEDIA_DEVICE_TOGGLE,
     payload,
   }
 }
 
-export function setAudioConstraint(
-  payload: AudioConstraint,
-): MediaAudioConstraintAction {
+export function setSizeConstraint(
+  payload: SizeConstraint | null,
+): MediaSizeConstraintAction {
   return {
-    type: MEDIA_AUDIO_CONSTRAINT_SET,
+    type: MEDIA_SIZE_CONSTRAINT,
     payload,
   }
+}
+
+export function setDeviceId(
+  payload: DeviceId,
+): MediaDeviceIdAction {
+  return {
+    type: MEDIA_DEVICE_ID,
+    payload,
+  }
+}
+
+export function setDeviceIdOrDisable(
+  payload: DeviceId,
+): MediaDeviceIdAction | MediaDeviceToggleAction {
+  if (payload.deviceId === DEVICE_DISABLED_ID) {
+    return toggleDevice({
+      kind: payload.kind,
+      enabled: false,
+    })
+  }
+
+  return setDeviceId(payload)
 }
 
 export const play = makeAction('MEDIA_PLAY', async () => {
@@ -147,11 +173,8 @@ export const play = makeAction('MEDIA_PLAY', async () => {
 })
 
 export type GetMediaTrackParams = {
-  kind: 'audio'
-  constraint: AudioConstraint
-} | {
-  kind: 'video'
-  constraint: VideoConstraint
+  kind: MediaKind
+  constraint: MediaTrackConstraints | false
 }
 
 export interface MediaTrackPayload {
@@ -211,7 +234,7 @@ export function enableMediaTrack(kind: MediaKind): MediaTrackEnableAction {
 
 export const getMediaStream = makeAction(
   MEDIA_STREAM,
-  async (constraints: GetMediaConstraints) => {
+  async (constraints: MediaStreamConstraints) => {
     if (!constraints.audio && !constraints.video) {
       const payload: AddLocalStreamPayload = {
         stream: new MediaStream(),
@@ -242,6 +265,26 @@ export const getDesktopStream = makeAction(
   },
 )
 
+// getDeviceId is a helper for figuring out the correct device ID.
+export function getDeviceId(
+  enabled: boolean,
+  constraint: MediaTrackConstraints,
+): string {
+  if (!enabled) {
+    return DEVICE_DISABLED_ID
+  }
+
+  if (typeof constraint.deviceId !== 'string') {
+    return DEVICE_DEFAULT_ID
+  }
+
+  return constraint.deviceId
+}
+
+export function getTracksByKind(stream: MediaStream, kind: MediaKind) {
+  return kind === 'video' ? stream.getVideoTracks() : stream.getAudioTracks()
+}
+
 export type MediaEnumerateAction = AsyncAction<'MEDIA_ENUMERATE', MediaDevice[]>
 export type MediaStreamAction =
   AsyncAction<'MEDIA_STREAM', AddLocalStreamPayload>
@@ -249,8 +292,9 @@ export type MediaPlayAction = AsyncAction<'MEDIA_PLAY', void>
 export type MediaTrackAction = AsyncAction<'MEDIA_TRACK', MediaTrackPayload>
 
 export type MediaAction =
-  MediaVideoConstraintAction |
-  MediaAudioConstraintAction |
+  MediaDeviceToggleAction |
+  MediaDeviceIdAction |
+  MediaSizeConstraintAction |
   MediaEnumerateAction |
   MediaStreamAction |
   MediaTrackAction |

commit c671a8cc095f50d63c97c03ce497b9ce07a34176

Fix media picker before joining the call
The value of an `<option>` element was incorrectly being set as a JSON
option. This bug was most likely introduced in b35fdb6b.

Also run `npm audit fix`.

Closes #228.

src/client/components/Media.tsx
@@ -266,7 +266,7 @@ function Options(props: OptionsProps) {
         .map(device =>
           <option
             key={device.id}
-            value={JSON.stringify({deviceId: device.id})}
+            value={device.id}
           >
             {device.name || device.type}
           </option>,

src/client/reducers/media.ts
@@ -124,7 +124,6 @@ export function handlePlay(
         autoplayError: false,
       }
     case 'rejected':
-      console.log('play rejected', action.payload.name)
       if (action.payload.name !== 'NotAllowedError') {
         return state
       }

src/client/components/Media.test.tsx
@@ -5,13 +5,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import TestUtils from 'react-dom/test-utils'
 import { Provider } from 'react-redux'
-import { createStore, Store } from '../store'
-import { Media } from './Media'
-import { MEDIA_ENUMERATE, ME, DIAL, DIAL_STATE_IN_CALL, DIAL_STATE_HUNG_UP } from '../constants'
 import { dial } from '../actions/CallActions'
-import { MediaStream } from '../window'
-
+import { DEVICE_DEFAULT_ID, DEVICE_DISABLED_ID, DIAL, DIAL_STATE_HUNG_UP, DIAL_STATE_IN_CALL, ME, MEDIA_ENUMERATE } from '../constants'
 import { MediaConstraint } from '../reducers/media'
+import { createStore, Store } from '../store'
+import { MediaStream } from '../window'
+import { Media } from './Media'
 
 describe('Media', () => {
 
@@ -27,8 +26,8 @@ describe('Media', () => {
         type: 'audioinput',
       }, {
         id: '456',
-        label: 'Video Input',
-        name: 'videoinput',
+        name: 'Video Input',
+        type: 'videoinput',
       }],
     })
   })
@@ -128,6 +127,42 @@ describe('Media', () => {
       expect(store.getState().media.dialState).toBe(DIAL_STATE_HUNG_UP)
       const nickname = store.getState().nicknames[ME]
       expect((dial as jest.Mock).mock.calls).toEqual([[ { nickname } ]])
+    })
+  })
+
+  describe('options', () => {
+    it('should populate audio options', async () => {
+      const node = await render()
+      const options = Array.from(
+        node.querySelectorAll('select[name=audio-input] option')!,
+      )
+      expect(options.map(o => o.getAttribute('value'))).toEqual([
+        DEVICE_DISABLED_ID,
+        DEVICE_DEFAULT_ID,
+        '123',
+      ])
+      expect(options.map(o => o.textContent!.trim())).toEqual([
+        'No Audio',
+        'Default Audio',
+        'Audio Input',
+      ])
+    })
+
+    it('should populate video options', async () => {
+      const node = await render()
+      const options = Array.from(
+        node.querySelectorAll('select[name=video-input] option')!,
+      )
+      expect(options.map(o => o.getAttribute('value'))).toEqual([
+        DEVICE_DISABLED_ID,
+        DEVICE_DEFAULT_ID,
+        '456',
+      ])
+      expect(options.map(o => o.textContent!.trim())).toEqual([
+        'No Video',
+        'Default Video',
+        'Video Input',
+      ])
     })
   })
 